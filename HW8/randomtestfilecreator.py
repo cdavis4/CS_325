d65e3a8602c3f2c0a5a3866776cfc774784aca46
cdavis4 <davicarr@oregonstate.edu>
2019-05-07 10:56:41 -0700

    changed code
    -created WriteToText class 
    - added better error message

--- HW8/randomtestfilecreator.py
+++ HW8/randomtestfilecreator.py
@@ -5,41 +5,42 @@ Function random creates random tests in file for binpack problem HW8
 """
 import os,sys, random
 
-def writeSameLine(inputText):
-    with open('binRand300.txt', "a") as fileOut:
-        fileOut.write(inputText)
+class WriteToText():
+    def __init__(self,inputFile):
+        self.text = ""
+        self.file = inputFile
+    def writeLine(self,inputText="", inChar=''):
+        self.text = str(inputText)
+        with open(self.file, "a") as fileOut:
+            fileOut.write(self.text)
+            if inChar == '\n' or inChar == '\t':
+                fileOut.write(inChar)
 
-def writeLine(inputText):
-    with open('binRand300.txt', "a") as fileOut:
-        fileOut.write(str(inputText))
-        fileOut.write('\n')
-
-def randomArray(size,low,high):
+def randomArray(size,low,high,inFile,filewrite):
     i = 0
     while i < size:
         randNum = random.randint(low,high)
-        writeSameLine((repr(randNum) + ' '),)
+        filewrite.writeLine((repr(randNum) + ' '),)
         i += 1
-    with open('binRand300.txt', "a") as fileOut:
-        fileOut.write('\n')
-
+    filewrite.writeLine('\n')
+    
 def main(n):
     try:
-        if os.path.exists('binRand300.txt'):
-            os.remove('binRand300.txt')
+        file = 'binRand300.txt'
+        if os.path.exists(file):
+            os.remove(file)
+        wfile = WriteToText(file)
         tests = 4
-        writeLine(tests)
+        wfile.writeLine(tests,'')
         i = 0
         while i < tests:
             c = 300
-            writeLine(c)
-            writeLine(n)
-            randomArray(n,1,c)
+            wfile.writeLine(c)
+            wfile.writeLine(n)
+            randomArray(n,1,c,file,wfile)
             i +=1
-
-    except:
-        print "fail"
-
+    except Exception as e:
+        print("fail: ",e)
 
 if __name__ == "__main__":
    main(300)
